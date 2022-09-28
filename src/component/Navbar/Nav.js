8c9aa2fc24930747f10e4d59fb91eae827a69c77
JunayedIslam1 <junayedjohny@gmail.com>
2022-09-29 00:02:44 +0600

    first-commit

---
+++ src/component/Navbar/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { FaCube } from "react-icons/fa";
+import './Nav.css'
+
+const Nav = () => {
+    return (
+        <div className='navbar'>
+
+            <h2 className='nav-text'> <FaCube className='icon'></FaCube>Dream Programming Club</h2>
+
+        </div>
+    );
+};
+
+export default Nav;
